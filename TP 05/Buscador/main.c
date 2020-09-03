823ed68051abe328121a79480ae1502095e715c4
Wen <wenceslaomateos@gmail.com>
2020-09-03 00:52:41 -0300

    arreglado problema en la muestra

--- TP 05/Buscador/main.c
+++ TP 05/Buscador/main.c
@@ -56,22 +56,22 @@ int main(int argsc, char *args1[])
     tipoPosting post[TAM_DIC];
     diccionario dic;
     dic.cantmax = TAM_DIC;
-    dic.palabra = (paldic *) malloc(dic.cantmax * sizeof(paldic));
+    dic.palabra = (paldic *)malloc(dic.cantmax * sizeof(paldic));
     strcpy(ruta, "./");
-    if(argsc == 2)
+    if (argsc == 2)
         strcpy(ruta, args1[1]);
     generaDicc(&dic, &rutas, ruta);
-    dic.palabra = (paldic *) realloc(dic.palabra, dic.cantpal * sizeof(paldic));
-    if(dic.cantpal != 0)
+    dic.palabra = (paldic *)realloc(dic.palabra, dic.cantpal * sizeof(paldic));
+    if (dic.cantpal != 0)
     {
         printf("Parseando ... 100%c listo\n", '%');
         burbuja(dic.palabra, 0, dic.cantpal);
         printf("Ordenando ... 100%c listo\n", '%');
         agrupamiento(&dic);
-        dic.palabra = (paldic *) realloc(dic.palabra, dic.cantpal * sizeof(paldic));
+        dic.palabra = (paldic *)realloc(dic.palabra, dic.cantpal * sizeof(paldic));
         printf("Agrupando ... 100%c listo\n", '%');
         dicPost(&dic, post);
-        dic.palabra = (paldic *) realloc(dic.palabra, dic.cantpal * sizeof(paldic));
+        dic.palabra = (paldic *)realloc(dic.palabra, dic.cantpal * sizeof(paldic));
         printf("Generando diccionario y posteando ... 100%c listo\n", '%');
         //menu(dic, post, rutas);
         generaFin(&dic, rutas, post);
@@ -85,18 +85,18 @@ void menu(diccionario dic, tipoPosting post[], truta rutas)
     int j, encontrado;
     stringpal palabra;
     op = 's';
-    while(toupper(op) != 'N')
+    while (toupper(op) != 'N')
     {
         printf("Ingrese la palabra a buscar :");
         scanf("%s", palabra);
-        for(j = 0; strlen(palabra) < j; j++)
+        for (j = 0; strlen(palabra) < j; j++)
             palabra[j] = tolower(palabra[j]);
         encontrado = busqueda(&dic, post, palabra);
-        if(encontrado != -1)
+        if (encontrado != -1)
         {
             j = dic.palabra[encontrado].RR;
             printf("\tEncontrada en el archivo : %s , %hu veces\n", rutas[post[j].D], post[j].F);
-            while(post[j].PR != -1)
+            while (post[j].PR != -1)
             {
                 j = post[j].PR;
                 printf("\tEncontrada en el archivo : %s , %hu veces\n", rutas[post[j].D], post[j].F);
@@ -115,23 +115,24 @@ void generaFin(diccionario *dic, truta rutas, tipoPosting post[])
     int i, j;
     FILE *arch;
     arch = fopen("diccionario.tex", "wt");
-    for(i = 0; i < dic->cantpal; i++){
+    for (i = 0; i < dic->cantpal; i++)
+    {
         fprintf(arch, "%s TD#: %d FT#: %d RR#: %d\n", dic->palabra[i].pal, dic->palabra[i].TD, dic->palabra[i].FT, dic->palabra[i].RR);
-        printf(arch, "%s TD#: %d FT#: %d RR#: %d\n", dic->palabra[i].pal, dic->palabra[i].TD, dic->palabra[i].FT, dic->palabra[i].RR);
+        printf("%s TD#: %d FT#: %d RR#: %d\n", dic->palabra[i].pal, dic->palabra[i].TD, dic->palabra[i].FT, dic->palabra[i].RR);
     }
     fclose(arch);
     arch = fopen("posting.tex", "wt");
     j = 0;
-    for(i = 0; i < dic->cantpal; i++)
+    for (i = 0; i < dic->cantpal; i++)
     {
         j = dic->palabra[i].RR;
         fprintf(arch, "\tRR: %d Archivo: %s F#: %hu PR#: %d\n", j, rutas[post[j].D], post[j].F, post[j].PR);
-        printf(arch, "\tRR: %d Archivo: %s F#: %hu PR#: %d\n", j, rutas[post[j].D], post[j].F, post[j].PR);
-        while(post[j].PR != -1)
+        printf("\tRR: %d Archivo: %s F#: %hu PR#: %d\n", j, rutas[post[j].D], post[j].F, post[j].PR);
+        while (post[j].PR != -1)
         {
             j = post[j].PR;
             fprintf(arch, "\tRR: %d Archivo: %s F#: %hu PR#: %d\n", j, rutas[post[j].D], post[j].F, post[j].PR);
-            printf(arch, "\tRR: %d Archivo: %s F#: %hu PR#: %d\n", j, rutas[post[j].D], post[j].F, post[j].PR);
+            printf("\tRR: %d Archivo: %s F#: %hu PR#: %d\n", j, rutas[post[j].D], post[j].F, post[j].PR);
         }
     }
     fclose(arch);
@@ -143,15 +144,15 @@ int busqueda(diccionario *dic, tipoPosting post[], stringpal palabra)
     inf = 0;
     sup = dic->cantpal - 1;
     centro = ((sup - inf) / 2) + inf;
-    while(inf <= sup && strcmp(dic->palabra[centro].pal, palabra) != 0)
+    while (inf <= sup && strcmp(dic->palabra[centro].pal, palabra) != 0)
     {
-        if(strcmp(dic->palabra[centro].pal, palabra) > 0)
+        if (strcmp(dic->palabra[centro].pal, palabra) > 0)
             sup = centro - 1;
         else
             inf = centro + 1;
         centro = ((sup - inf) / 2) + inf;
     }
-    if(strcmp(dic->palabra[centro].pal, palabra) == 0)
+    if (strcmp(dic->palabra[centro].pal, palabra) == 0)
         return centro;
     else
         return -1;
@@ -160,12 +161,12 @@ int busqueda(diccionario *dic, tipoPosting post[], stringpal palabra)
 void muestra(diccionario *dic, truta rutas, tipoPosting post[])
 {
     int i, j;
-    for(i = 0; i < dic->cantpal; i++)
+    for (i = 0; i < dic->cantpal; i++)
     {
         printf("Palabra: %s TotArch: %hu FrecTotal: %hu RR: %d\n", dic->palabra[i].pal, dic->palabra[i].TD, dic->palabra[i].FT, dic->palabra[i].RR);
         j = dic->palabra[i].RR;
         printf("\tArchivo: %s Frecuencia: %hu PR: %d\n", rutas[post[j].D], post[j].F, post[j].PR);
-        while(post[j].PR != -1)
+        while (post[j].PR != -1)
         {
             j = post[j].PR;
             printf("\tArchivo: %s Frecuencia: %hu PR: %d\n", rutas[post[j].D], post[j].F, post[j].PR);
@@ -183,9 +184,9 @@ void dicPost(diccionario *dic, tipoPosting post[])
     post[0].PR = -1;
     dic->palabra[0].TD = 1;
     dic->palabra[0].RR = 0;
-    for(i = 1; i < dic->cantpal; i++)
+    for (i = 1; i < dic->cantpal; i++)
     {
-        if(strcmp(dic->palabra[i].pal, dic->palabra[k].pal) == 0) //encuentra 2 palabras iguales en diccionario
+        if (strcmp(dic->palabra[i].pal, dic->palabra[k].pal) == 0) //encuentra 2 palabras iguales en diccionario
         {
             dic->palabra[k].FT += dic->palabra[i].FT;
             dic->palabra[k].TD++;
@@ -213,9 +214,9 @@ void agrupamiento(diccionario *dic)
     unsigned short int i, j;
     i = 1;
     j = 0;
-    while(i < dic->cantpal)
+    while (i < dic->cantpal)
     {
-        if(strcmp(dic->palabra[i].pal, dic->palabra[j].pal) == 0 && dic->palabra[i].TD == dic->palabra[j].TD)
+        if (strcmp(dic->palabra[i].pal, dic->palabra[j].pal) == 0 && dic->palabra[i].TD == dic->palabra[j].TD)
             (dic->palabra[j].FT)++;
         else
         {
@@ -242,21 +243,21 @@ void generaDicc(diccionario *dic, truta *rutas, char *ruta)
     dir = opendir(ruta);
     dic->cantpal = 0;
     /* Miramos que no haya error */
-    if(dir == NULL)
+    if (dir == NULL)
         printf("No se pudo abrir el directorio\n");
     else
     {
         /* Leyendo uno a uno todos los archivos que hay */
-        while((ent = readdir(dir)) != NULL)
+        while ((ent = readdir(dir)) != NULL)
         {
             strcpy(file, ent->d_name);
-            if(strcmp(file + strlen(file) - 4, ".txt") == 0)
+            if (strcmp(file + strlen(file) - 4, ".txt") == 0)
             {
                 strcpy((*rutas)[numdir], ruta);
                 strcat((*rutas)[numdir], file);
                 parsing((*rutas)[numdir], dic, numdir);
                 numdir++;
-                if(numdir >= numax)
+                if (numdir >= numax)
                 {
                     numax += TAM_DIC;
                     *rutas = (truta)realloc((*rutas), (numdir + 1) * sizeof(stringarch));
@@ -266,7 +267,7 @@ void generaDicc(diccionario *dic, truta *rutas, char *ruta)
         *rutas = (truta)realloc((*rutas), (numdir + 1) * sizeof(stringarch));
         printf("%d archivo(s) encontrado(s)\n", numdir);
     }
-    if(numdir == 0)
+    if (numdir == 0)
     {
         printf("El directorio estavacio\n");
     }
@@ -280,25 +281,25 @@ void parsing(char *file, diccionario *dic, unsigned short int numdir)
     char aux[TAM_PAL];
     unsigned short int i;
     arch = fopen(file, "rt");
-    while(!feof(arch))
+    while (!feof(arch))
     {
         i = 0;
         fscanf(arch, "%c", &(aux[i]));
         aux[i] = tolower(aux[i]);
-        while(aux[i] <= 122 && aux[i] >= 97 && !feof(arch))
+        while (aux[i] <= 122 && aux[i] >= 97 && !feof(arch))
         {
             i++;
             fscanf(arch, "%c", &(aux[i]));
             aux[i] = tolower(aux[i]);
         }
-        if(i != 0)
+        if (i != 0)
         {
             aux[i] = '\0';
             dic->cantpal++;
-            if(dic->cantpal >= dic->cantmax)
+            if (dic->cantpal >= dic->cantmax)
             {
                 dic->cantmax += TAM_DIC;
-                dic->palabra = (paldic *) realloc(dic->palabra, dic->cantmax * sizeof(paldic));
+                dic->palabra = (paldic *)realloc(dic->palabra, dic->cantmax * sizeof(paldic));
             }
             strcpy((dic->palabra[(dic->cantpal - 1)].pal), aux);
             dic->palabra[(dic->cantpal - 1)].TD = numdir;
@@ -312,8 +313,8 @@ int particion(paldic arr[], int inf, int sup)
 {
     int i = (inf - 1);
     paldic aux;
-    for(int j = inf; j <= sup - 1; j++)
-        if(strcmp(arr[j].pal, arr[sup].pal) > 0 || (strcmp(arr[j].pal, arr[sup].pal) == 0 && arr[j].TD > arr[sup].TD))
+    for (int j = inf; j <= sup - 1; j++)
+        if (strcmp(arr[j].pal, arr[sup].pal) > 0 || (strcmp(arr[j].pal, arr[sup].pal) == 0 && arr[j].TD > arr[sup].TD))
         {
             i++;
             aux = arr[i];
@@ -328,7 +329,7 @@ int particion(paldic arr[], int inf, int sup)
 
 void quickSort(paldic A[], int inf, int sup)
 {
-    if(inf < sup)
+    if (inf < sup)
     {
         int p = particion(A, inf, sup);
         quickSort(A, inf, p - 1);
@@ -340,9 +341,9 @@ void burbuja(paldic Vec[], unsigned int inicio, unsigned int fin)
 {
     int i, j;
     paldic aux;
-    for(i = 0; i < fin; i++)
-        for(j = i; j < fin; j++)
-            if(strcmp(Vec[i].pal, Vec[j].pal) > 0 || (strcmp(Vec[i].pal, Vec[j].pal) == 0 && Vec[i].TD > Vec[j].TD))
+    for (i = 0; i < fin; i++)
+        for (j = i; j < fin; j++)
+            if (strcmp(Vec[i].pal, Vec[j].pal) > 0 || (strcmp(Vec[i].pal, Vec[j].pal) == 0 && Vec[i].TD > Vec[j].TD))
             {
                 aux = Vec[i];
                 Vec[i] = Vec[j];
